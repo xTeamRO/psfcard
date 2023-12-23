7aeb87ca01c47e6214fb79b2a2e0911e5d397ceb
xTeamRO <154669340+xTeamRO@users.noreply.github.com>
2023-12-23 23:25:49 +0530

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const path = require("path");
 app.use(express.urlencoded({extended:true}));
 app.use(express.json());
 app.get("/",(req,res)=>{
-    res.sendFile(path.join(__dirname,"index.js"))
+    res.sendFile(path.join(__dirname,"index.html"))
 })
 app.get("*",(req,res)=>{
      res.send("<h2 style='text-align:center;'>page not found<h2>")
